2b37622d7f796b5b8e9b6bf23fff60e8a6824e06
Asude Nisa Tıraş <asudenisatiras@gmail.com>
2023-05-15 20:36:34 +0300

    Search button added.

--- SyncNews/SyncNews/Controllers/ViewController.swift
+++ SyncNews/SyncNews/Controllers/ViewController.swift
@@ -12,13 +12,16 @@ import SyncNewsAPI
 var news = [Welcome]()
 var title : Welcome?
 
-class ViewController: UIViewController, LoadingShowable {
+class ViewController: UIViewController, LoadingShowable,UISearchBarDelegate {
     
     @IBOutlet weak var newsTableView: UITableView!
+    
+    @IBOutlet weak var searchButton: UISearchBar!
     let service: TopNewsServiceProtocol = TopNewsService()
-    
+    private var filteredNews: [Welcome] = []
     private var news: [Welcome] = []
-    
+    private var isSearchResultEmpty: Bool = false
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //fetchNews()
@@ -27,6 +30,7 @@ class ViewController: UIViewController, LoadingShowable {
     override func viewDidLoad() {
         super.viewDidLoad()
         newsTableView.register(cellType: NewsTableViewCell.self)
+        searchButton.delegate = self
         fetchNews()
         
         
@@ -44,13 +48,81 @@ class ViewController: UIViewController, LoadingShowable {
             case .success(let news):
                 print("asude: \(news)")
                 self.news = news
+                self.filteredNews = news
                 self.newsTableView.reloadData()
             case .failure(let error):
                 print("Asude: \(error)")
             }
         }
     }
+    func searchNews(with searchText: String) {
+        if searchText.isEmpty {
+            filteredNews = news
+        } else {
+            filteredNews = news.filter { $0.title?.range(of: searchText, options: [.caseInsensitive, .diacriticInsensitive]) != nil }
+        }
+        
+        isSearchResultEmpty = filteredNews.isEmpty
+        newsTableView.reloadData()
+    }
+
+      
+      func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+          searchNews(with: searchText)
+      }
+      
+      func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+          searchBar.resignFirstResponder() // Klavyeyi kapat
+      }
+
+            }
+
+              
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearchResultEmpty {
+                return 1 // Bulunamadı hücresi için 1 satır
+            } else {
+                return filteredNews.count
+            }
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if isSearchResultEmpty {
+               let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+               
+               let noResultsImage = UIImage(named: "NoResultFound")
+               let imageView = UIImageView(image: noResultsImage)
+               
+            imageView.contentMode = .scaleToFill
+               imageView.translatesAutoresizingMaskIntoConstraints = false
+               
+               cell.contentView.addSubview(imageView)
+               
+               NSLayoutConstraint.activate([
+                   imageView.centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor),
+                   imageView.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+                   imageView.widthAnchor.constraint(equalToConstant: 200),
+                   imageView.heightAnchor.constraint(equalToConstant: 200)
+               ])
+               
+               return cell
+           } else {
+               let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
+               cell.configure(news: filteredNews[indexPath.row])
+               return cell
+           }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        let selectedNews = filteredNews[indexPath.row] // Filtrelenmiş haberlerden seçileni al
+                performSegue(withIdentifier: "nextPageSegue", sender: selectedNews)
+        
+         
+        }
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "nextPageSegue" {
             if let nextViewController = segue.destination as? DetailsViewController,
@@ -68,37 +140,6 @@ class ViewController: UIViewController, LoadingShowable {
                         }
                     }
             }
-           
-
-            
-
-            }
-
-              
-
-
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return news.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
-        cell.configure(news: news[indexPath.row])
-        
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
-            let selectedNews = news[indexPath.row]
-            performSegue(withIdentifier: "nextPageSegue", sender: selectedNews)
-        
-         
-        }
-   
-    
-
-
     }
 
     
